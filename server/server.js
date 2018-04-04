e1c93cecb626abe5c1cf2d7a57ee1c933cd1ae90
yappps <yapyuanxin@gmail.com>
2018-04-04 23:14:59 +0800

    added a single route: GET /cities in server.js & added babel config in server package.json

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-// This will be the entry file for server 
+// This will be the entry file for server
+import bodyParser from "body-parser";
+import express from "express";
+const app = express();
+
+app.use(bodyParser.json()); // [Q] .json()
+app.use(bodyParser.urlencoded({ extended: false })); // [Q] why need to .urlencoded()
+
+const router = express.Router();
+router.get("/cities", (req, res) => {
+  const cities = [
+    { name: "NYC", population: 1111 },
+    { name: "LA", population: 2222 },
+    { name: "SHA", population: 3333 }
+  ];
+  res.json(cities);
+});
+app.use(router);
+
+app.set("port", process.env.PORT || 3001);
+app.listen(app.get("port"), () => {
+  console.log(`You are Listening on ${app.get("port")}`);
+});
